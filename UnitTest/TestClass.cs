d09bf0e92fe84a78cbc9a8c3ec71965ebd79040c
allandemancilha <allandemancilha@gmail.com>
2021-01-19 18:57:52 -0300

    Minha Primeira Teoria Com Xunit

--- UnitTest/TestClass.cs
+++ UnitTest/TestClass.cs
@@ -4,6 +4,20 @@ using UnitTest;
 
 public class TestClass
 {
+    /* 
+        Theory: Uma Teoria Executa o Mesmo Teste Utilizando Uma Série de Parâmetros Definidos, Quando 
+        Algum Parâmetro Não Passa no Teste, a Teoria é Considerada Falha.
+    */
+    [Theory]
+    // InlineData: Utilizado Para Definir os Dados Que Serão Utilizados Como Parâmetro Pela Teoria
+    [InlineData(5)]
+    [InlineData(11)]
+    [InlineData(19)]
+    public void MyFirstTheory(int value)
+    {
+        Assert.True(Program.IsOdd(value));
+    }
+
     // Fact: O Atributo Declara um Método de Teste Que é Executado Pelo Executor de Teste.
     [Fact]
     public void PassingAddTest()
