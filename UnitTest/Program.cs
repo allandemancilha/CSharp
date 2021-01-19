bcd9af7a09afd1d65d7719bd8442355b96ed187e
allandemancilha <allandemancilha@gmail.com>
2021-01-19 14:42:47 -0300

    Adicionando o Pacote Microsoft.NET.Test.Sdk

--- UnitTest/Program.cs
+++ UnitTest/Program.cs
@@ -6,7 +6,10 @@ namespace UnitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sumResult = Add(5, 6);
+
+            Console.WriteLine(sumResult);
+            Console.WriteLine(IsOdd(sumResult));
         }
 
         public static int Add(int x, int y)
