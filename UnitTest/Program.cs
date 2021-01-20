23a6680b9542527779a92ee457c44f6240dd65c1
allandemancilha <allandemancilha@gmail.com>
2021-01-20 13:28:10 -0300

    Adicionando Comentários

--- UnitTest/Program.cs
+++ UnitTest/Program.cs
@@ -12,11 +12,16 @@ namespace UnitTest
             Console.WriteLine(IsOdd(sumResult));
         }
 
+        // Add: O Método Recebe Dois Números Inteiros Como Parâmetro e Retorna o Valor da Soma
         public static int Add(int x, int y)
         {
             return x + y;
         }
         
+        /* 
+            IsOdd: O Método Recebe um Número Inteiro Como Parâmetro e Verifica se o Número é Ímpar, Ca-
+            so o Número Seja Ímpar é Retornado true
+        */
         public static bool IsOdd(int value)
         {
             return value % 2 == 1;
