7781c097975448fd2dde5341cd1c26aa0fd31d39
allandemancilha <allandemancilha@gmail.com>
2021-01-21 15:46:50 -0300

    Criando CategoryController.cs

---
+++ SimpleAPI/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleAPI.Data;
+using SimpleAPI.Models;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("v1/categories")]
+    public class CategoryController : ControllerBase
+    {
+        
+    }
+}
